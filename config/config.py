a8e137ca28fbe20a70e7a1c7f114c86e505f076f
S-SIRIUS <sirius5b8b@gmail.com>
2024-02-06 00:01:21 +0900

    [fix] file path

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
 import os
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = ''
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-PINECONE_API_KEY = "c6177ec6-bfcd-4271-83e0-d9b2cfdb9fcb"
+PINECONE_API_KEY = ""
